5d3c835c6cb533f6cdd28bbd4e0b1a9e686e6df9
abhi0516 <90794084+abhi0516@users.noreply.github.com>
2022-08-24 12:55:50 -0500

    working on trig functions commit

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -21,9 +21,9 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt(number1ET.getText().toString());
-        int num2 = Integer.parseInt(number2ET.getText().toString());
-        int sum = num1 + num2;
+        double num1 = Double.parseDouble(number1ET.getText().toString());
+        double num2 = Double.parseDouble(number2ET.getText().toString());
+        double sum = num1 + num2;
 
         numberSumTV.setText("" + sum);
     }
@@ -33,11 +33,11 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt(number1ET.getText().toString());
-        int num2 = Integer.parseInt(number2ET.getText().toString());
-        int sum = num1 - num2;
+        double num1 = Double.parseDouble(number1ET.getText().toString());
+        double num2 = Double.parseDouble(number2ET.getText().toString());
+        double diff = num1 - num2;
 
-        numberSumTV.setText("" + sum);
+        numberSumTV.setText("" + diff);
     }
 
     public void findProduct(View view) {
@@ -45,11 +45,11 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt(number1ET.getText().toString());
-        int num2 = Integer.parseInt(number2ET.getText().toString());
-        int sum = num1 * num2;
+        double num1 = Double.parseDouble(number1ET.getText().toString());
+        double num2 = Double.parseDouble(number2ET.getText().toString());
+        double product = num1 * num2;
 
-        numberSumTV.setText("" + sum);
+        numberSumTV.setText("" + product);
     }
 
     public void findQuotient(View view) {
@@ -57,12 +57,25 @@ public class MainActivity extends AppCompatActivity {
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
 
-        int num1 = Integer.parseInt(number1ET.getText().toString());
-        int num2 = Integer.parseInt(number2ET.getText().toString());
-        int sum = num1 / num2;
+        double num1 = Double.parseDouble(number1ET.getText().toString());
+        double num2 = Double.parseDouble(number2ET.getText().toString());
+        double quotient = num1 / num2;
 
-        numberSumTV.setText("" + sum);
+        numberSumTV.setText("" + quotient);
     }
+
+
+//    public void findSin(View view) {
+//        EditText number1ET = findViewById(R.id.num1ET);
+//
+//        TextView numberSumTV = findViewById(R.id.resultTV);
+//
+//        int num1 = int.parseInt(trig.getText().toString());
+//
+//        double quotient = num1 / num2;
+//
+//        numberSumTV.setText("" + quotient);
+//    }
 
 
 }
