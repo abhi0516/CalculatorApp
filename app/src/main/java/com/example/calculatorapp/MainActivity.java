aff95ad118b87f39587b1b5fc85cb02f15a10e4f
abhi0516 <90794084+abhi0516@users.noreply.github.com>
2022-08-25 12:51:34 -0500

    trig functions working commit

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -55,27 +55,56 @@ public class MainActivity extends AppCompatActivity {
     public void findQuotient(View view) {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
-        TextView numberSumTV = findViewById(R.id.resultTV);
+        TextView numberQuotientTV = findViewById(R.id.resultTV);
 
         double num1 = Double.parseDouble(number1ET.getText().toString());
         double num2 = Double.parseDouble(number2ET.getText().toString());
         double quotient = num1 / num2;
 
-        numberSumTV.setText("" + quotient);
+        numberQuotientTV.setText("" + quotient);
     }
 
 
-//    public void findSin(View view) {
-//        EditText number1ET = findViewById(R.id.num1ET);
-//
-//        TextView numberSumTV = findViewById(R.id.resultTV);
-//
-//        int num1 = int.parseInt(trig.getText().toString());
-//
-//        double quotient = num1 / num2;
-//
-//        numberSumTV.setText("" + quotient);
-//    }
+    public void findSin(View view) {
+        EditText trigDegreesET = findViewById(R.id.trigDegreesET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
 
+        int degrees = Integer.parseInt(trigDegreesET.getText().toString());
+
+        double radians = Math.toRadians(degrees);
+
+        // sin() method to get the sine value
+        double sinValue = Math.sin(radians);
+
+        numberSumTV.setText("sin(" + degrees + ") = " + sinValue);
+    }
+
+    public void findCos(View view) {
+        EditText trigDegreesET = findViewById(R.id.trigDegreesET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int degrees = Integer.parseInt(trigDegreesET.getText().toString());
+
+        double radians = Math.toRadians(degrees);
+
+        // cos() method to get the sine value
+        double cosValue = Math.cos(radians);
+
+        numberSumTV.setText("cos(" + degrees + ") = " + cosValue);
+    }
+
+    public void findTan(View view) {
+        EditText trigDegreesET = findViewById(R.id.trigDegreesET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int degrees = Integer.parseInt(trigDegreesET.getText().toString());
+
+        double radians = Math.toRadians(degrees);
+
+        // tan() method to get the sine value
+        double tanValue = Math.tan(radians);
+
+        numberSumTV.setText("tan(" + degrees + ") = " + tanValue);
+    }
 
 }
